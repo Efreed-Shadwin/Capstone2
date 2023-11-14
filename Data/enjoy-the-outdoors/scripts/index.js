e5bb86c65b3371340557df0e6c933c2fa98fcdfc
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-11-14 14:38:29 -0600

    added banner image a new font and some text about the org

---
+++ Data/enjoy-the-outdoors/scripts/index.js
@@ -0,0 +1,5 @@
+"use strict"
+function parkData(locationID,locationName,Address,city,state,zipCode,phone,fax,latitude,longitude,location){
+    for(let i=0;i<nationalParksArray.length;i++)
+    parkData 
+}
