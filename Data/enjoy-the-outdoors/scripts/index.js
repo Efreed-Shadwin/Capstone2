26df63dae77e851162d9a0fdb991e7b383aa3a84
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-11-16 10:50:45 -0600

    Added background

--- Data/enjoy-the-outdoors/scripts/index.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-function parkData(locationID,locationName,Address,city,state,zipCode,phone,fax,latitude,longitude,location){
-    for(let i=0;i<nationalParksArray.length;i++)
-    parkData 
-}
