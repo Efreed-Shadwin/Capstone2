a1af63bc3dc5cdd023034d459dd381f92e7821cd
Efreed-Shadwin <146854297+Efreed-Shadwin@users.noreply.github.com>
2023-11-17 08:03:59 -0600

    Cleaned up the national park variable names

--- scripts/nationalParks.js
+++ scripts/nationalParks.js
@@ -1,38 +1,38 @@
 let selectedParkType = "";
 let selectedLocation = "";
-document.addEventListener("DOMContentLoaded", e=> {
+document.addEventListener("DOMContentLoaded", createParkCard=> {
     parkTypesArray.forEach(pt => parkTypeList.innerHTML += `<option>${pt}</option>`);
     locationsArray.forEach(location => locationsList.innerHTML += `<option>${location}</option>`);
-    parkTypeList.addEventListener("change", e => {
-        selectedParkType = e.target.value;
+    parkTypeList.addEventListener("change", npt => {
+        selectedParkType = npt.target.value;
         showResults();
      });
-    locationsList.addEventListener("change", e => {
-        selectedLocation = e.target.value;
+    locationsList.addEventListener("change", npl => {
+        selectedLocation = npl.target.value;
         showResults();
     });
-    function isMatch(p) {
-        return (selectedLocation == "" || p.State == selectedLocation) &&
-            (selectedParkType == "" || p.LocationName.includes(selectedParkType))
+    function isMatch(npd) {
+        return (selectedLocation == "" || npd.State == selectedLocation) &&
+            (selectedParkType == "" || npd.LocationName.includes(selectedParkType))
     }
-    function getCard(p) {
+    function getCard(npd) {
         //allows flexibility for the phone fax and website
-        let phoneNumber = `Phone number: ${p.Phone}`;
-        let faxNumber = `Fax number: ${p.Fax}`;
-        let web = `For more info <a href="${p.Visit}" target="_blank">${p.Visit}</a>`;
-        if (p.Phone != 0) {
+        let phoneNumber = `Phone number: ${npd.Phone}`;
+        let faxNumber = `Fax number: ${npd.Fax}`;
+        let web = `For more info <a href="${npd.Visit}" target="_blank">${npd.Visit}</a>`;
+        if (npd.Phone != 0) {
             phone = `<h4>${phoneNumber}  `
         }
         else {
             phone = '<h4>Phone number: No number listed  '
         }
-        if (p.Fax != 0) {
+        if (npd.Fax != 0) {
             fax = `${faxNumber}</h4>`
         }
         else {
             fax = 'Fax number: No number listed</h4>'
         }
-        if (p.Visit != undefined) {
+        if (npd.Visit != undefined) {
             web = `<h4>${web}</h4>`
         }
         else {
@@ -40,21 +40,21 @@ document.addEventListener("DOMContentLoaded", e=> {
         }
         let contactInfo = phone + fax + web
         return `<div class="card park-card bg-warning-subtle">
-                    <h2 class="card-header">${p.LocationName}</h2>
+                    <h2 class="card-header">${npd.LocationName}</h2>
                     <div class="card=text">
                         <h3>
-                            <b>${p.State}</b>
+                            <b>${npd.State}</b>
                         </h3>
                     </div>
-                    <h4>${p.Address}, ${p.City}, ${p.ZipCode}</h4>
-                    <h4>Location ID: <a href="https://www.google.com/maps/search/?api=1&query=${p.Latitude},${p.Longitude}" target="_blank">${p.LocationID}</a> </h4>
-                    <h4>Coordinates: ${p.Longitude}, ${p.Latitude}</h4>${contactInfo}
+                    <h4>${npd.Address}, ${npd.City}, ${npd.ZipCode}</h4>
+                    <h4>Location ID: <a href="https://www.google.com/maps/search/?api=1&query=${npd.Latitude},${npd.Longitude}" target="_blank">${npd.LocationID}</a> </h4>
+                    <h4>Coordinates: ${npd.Longitude}, ${npd.Latitude}</h4>${contactInfo}
                 <div>`;
     }
     function showResults() {
         results.innerHTML = ""
         const filtered = nationalParksArray.filter(isMatch);
-        filtered.forEach(p => results.innerHTML += getCard(p));
+        filtered.forEach(npd => results.innerHTML += getCard(npd));
     }
     showResults();
 });
